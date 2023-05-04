a224cfddc8473073033203302a061cb1568762be
user <undyin0@gmail.com>
2023-05-04 21:07:14 +0530

    adapter

--- app/src/main/java/com/example/appointment/adapters/EmpAdapter.kt
+++ app/src/main/java/com/example/appointment/adapters/EmpAdapter.kt
@@ -1,4 +1,26 @@
 package com.example.appointment.adapters
 
-class EmpAdapter {
+import android.view.View
+import android.view.ViewGroup
+import androidx.recyclerview.widget.RecyclerView
+import com.example.appointment.models.Model
+
+class EmpAdapter (private val empList: ArrayList<Model> ) :
+    RecyclerView.Adapter<EmpAdapter.ViewHolder> () {
+
+    class ViewHolder (ItemView: View) : RecyclerView.ViewHolder(ItemView) {
+
+    }
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ViewHolder {
+        TODO("Not yet implemented")
+    }
+
+    override fun getItemCount(): Int {
+        TODO("Not yet implemented")
+    }
+
+    override fun onBindViewHolder(holder: ViewHolder, position: Int) {
+        TODO("Not yet implemented")
+    }
 }
