9be3d85077f2503036019146037950d6c9483478
user <undyin0@gmail.com>
2023-05-05 15:43:25 +0530

    working with fetching

--- app/src/main/java/com/example/appointment/activities/MainActivity.kt
+++ app/src/main/java/com/example/appointment/activities/MainActivity.kt
@@ -27,7 +27,7 @@ class MainActivity : ComponentActivity() {
             startActivity(intent)
         }
         btnFetchData.setOnClickListener{
-            val intent = Intent(this, InsertionActivity::class.java)
+            val intent = Intent(this, FetchingActivity::class.java)
             startActivity(intent)
         }
     }
